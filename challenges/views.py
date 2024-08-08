aadd35104d861d027c3ba5eb1f5520b7f0cc5a64
Xavier Sanchez <xavier_159_355@hotmail.com>
2024-08-08 18:07:09 -0500

    segunda version

--- challenges/views.py
+++ challenges/views.py
@@ -4,17 +4,27 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
+monthly_challenges = {
+    "january": "Practice sport at leat 20 min",
+    "february": "Dont smoke 1 box cigarettes per day",
+    "march": "Practice Djang 20 min at leat per day",
+    "april": "Practice git at least 20 min per day",
+    "may": "Go walk at least 20 min per day,rememmber is you HB",
+    "june": "Try to reach the divine medal in Dota 2",
+    "july": "Try yo improve in your project",
+    "september": "Go out with your girlfriend to new places",
+    "october": "Go and drink some beers at weeked",
+    "november": "Buy some things to your family what end of year partys are near",
+    "december": "Spend quality time with your family"
+}
+
 def monthly_challenge_by_number(request, month):
     return HttpResponse(month)
 
+
 def month_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Practice sport at leat 20 min"
-    elif month == "february":
-        challenge_text = "Dont smoke 1 box cigarettes per day"
-    elif month == "march":
-        challenge_text = "Practice Djang 20 min at leat per day"
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not a valid")
-    return HttpResponse(challenge_text)
--- practices-view.py
+++ practices-view.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+# from django.shortcuts import render
+# from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+# # Create your views here.
 
 
-def month_challenge(request, month):
-    month_text = None
-    months = ["january", "february", "march", "april", "may", "june", "september", "october", "november",
-              "december"
-              ]
-    challenge_text = ["Practice sport at leat 20 min",
-               "Dont smoke 1 box cigarettes per day",
-               "Practica Djang 20 min at leat per day"]
+# def month_challenge(request, month):
+#     month_text = None
+#     months = ["january", "february", "march", "april", "may", "june", "september", "october", "november",
+#               "december"
+#               ]
+#     challenge_text = ["Practice sport at leat 20 min",
+#                "Dont smoke 1 box cigarettes per day",
+#                "Practica Djang 20 min at leat per day"]
 
-    if month in months:
-        month_text = month
-        return challenge_text[months.index(month_text)]
+#     if month in months:
+#         month_text = month
+#         return challenge_text[months.index(month_text)]
 
-    return HttpResponse(month_text)
+#     return HttpResponse(month_text)
+
+dict1_= {
+    '1':"probando",
+    '2': "Seguimos probando",
+    '3': "seguimos con la gozadera"
+}
+
+print(dict1_)
