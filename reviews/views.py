3dc7f3f0630215e5c1da9ea866775fe5ada551d7
Xavier Sanchez <xavier_159_355@hotmail.com>
2024-11-27 22:51:50 -0500

    Forms complete

---
+++ reviews/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import ReviewForm
+from django.views import View
+# from .models import Review
+
+# Create your views here.
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+        
+    def post(self, request):
+        form = ReviewForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html", {
+        "has_error": False
+    })
